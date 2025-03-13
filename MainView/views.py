a8d35a59f0cd19977a28fb147e9f9c6ef08b69a6
hudson <h0915094@gmail.com>
2025-03-13 11:30:06 +0800

    add index.html, FBV return index.html

--- MainView/views.py
+++ MainView/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+# # Create your views here.
+# class MainView(APIView):
+#     def get(self, request):
+#         return Response({})
